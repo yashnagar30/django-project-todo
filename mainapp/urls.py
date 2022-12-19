c7332aea2c5751aad2564c17be4b75d879158872
Tushar <tushargangurde405@gmail.com>
2022-12-20 00:27:41 +0530

    changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     ####################give id no. item_id name or item_id=i.id ############
     # pass item_id as primary key to remove that the todo with given id
     path('del/<str:item_id>', views.remove, name="del"),
-    ########################################################################
-    path('admin/', admin.site.urls),
 ]
